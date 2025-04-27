25389a61e8bdb84ce0a36c426c6d70ada712a67c
Kelly <kellyk2603@gmail.com>
2025-04-26 21:53:15 -0700

    update home page

--- src/components/Home/TeamIntro.js
+++ src/components/Home/TeamIntro.js
@@ -76,28 +76,30 @@ const TeamIntro = () => {
             <p className="subtitle">Learn valuable skills that are directly transferable to the industry</p>
             <div className="skills-container">
                 <div className="SW-container col-container">
-                    <h5>SolidWorks</h5>
+                    <h5>Design</h5>
                     <p>
-                        You will get to learn SolidWorks, an industry standard cading 
-                        software that we use to design our robots.
+                        The design team collaborate on initial ideas through hand-drawn sketches and concept discussions before developing detailed CAD models in SOLIDWORKS and other design software. These models are transformed into precise prototypes, often 3D printed for rapid testing and refinement. Through iterative feedback, designs are improved to ensure optimal performance before final manufacturing.
                     </p>
                     <img className="img" src={Img3} alt="solidworks" />
                 </div>
                 <div className="STM-container col-container">
-                    <h5>STM32</h5>
+                    <h5>Fabrication</h5>
                     <p>
-                        Our robots run on the STM32 platform, which takes care of CAN 
-                        bus communication, PID calculations, IMU & magnometer interfacing, 
-                        etc. concurrently via the use of FreeRTOS.
+                        The fabrication process at Ascension Robotics brings designs to life through waterjet cutting, CNC machining, and resin molding. Engineers gain hands-on experience assembling parts with professional tools, as well as developing electronics through PCB soldering, control system integration, and precise wiring management to ensure reliable robot performance.
                     </p>
                     <img className="img" src={Img4} alt="VexU" />
                 </div>
                 <div className="PL-container col-container">
-                    <h5>Programming</h5>
+                    <h5>Embedded Programming</h5>
                     <p>
-                        We use a variety of programming languages to code our robots 
-                        including C#. From the robot's movement to its sensors, you will 
-                        get to learn a variety of ways to program functions of a robot.
+                    The embedded controls team programs motor systems using STM32CubeIDE and deploys them onto custom controllers for smooth, precise swerve-drive movement. Engineers implement anti-jam routines and PID loops for consistent shooter performance, while also customizing the in-game overlay UI to give drivers real-time feedback during matches.
+                    </p>
+                    <img className="img" src={Img5} alt="VexU" />
+                </div>
+                <div className="PL-container col-container">
+                    <h5>Camera Vision</h5>
+                    <p>
+                    The camera vision team develops machine learning models to detect robots and navigate complex environments. Trained over dozens of epochs, the system identifies enemy units, field locations, and key targets. Vision control enables automatic aiming, while autonomous navigation and shooting allow the Sentry robot to operate independently, having both defense and offense capabilities.
                     </p>
                     <img className="img" src={Img5} alt="VexU" />
                 </div>
@@ -106,29 +108,28 @@ const TeamIntro = () => {
         </div>
 
         {/* ================ Sponsors section =============== */}
-        <div className="section-container center">
-        <h4>Our Sponsors</h4>
-        <p className="subtitle">Email us at sfurc@sfu.ca to become the first to put your name here</p>
-        {/* <div className="sponsors-container">
-            <div className="sp">
-                <a href="https://humaninmotion.com/" target="_blank">
-                    <img src={Img6} alt="HumanInMotion" style={{ width: "300px", height: "auto" }} />
-                </a>
-            </div>
-            <div className="sp">
-                <a href="https://www.solidworks.com/" target="_blank">
-                    <img src={Img7} alt="SolidWorks" style={{ width: "300px", height: "auto" }} />
-                </a>
-            </div>
-            <div className="sp">
-                <a href="https://www.solidworks.com/" target="_blank">
-                    <img src={Img8} alt="Altium" style={{ width: "300px", height: "auto" }} />
-                </a>
-            </div>
-        </div> */}
-        {/* <button className="btn">Sign up to be one</button> */}
-    </div>
-
+        <div className="section-container sponsor-main-container center">
+            <h4>Our Sponsors</h4>
+            <p className="subtitle">Email us at sfurc@sfu.ca to become the first to put your name here</p>
+            {/* <div className="sponsors-container">
+                <div className="sp">
+                    <a href="https://humaninmotion.com/" target="_blank">
+                        <img src={Img6} alt="HumanInMotion" style={{ width: "300px", height: "auto" }} />
+                    </a>
+                </div>
+                <div className="sp">
+                    <a href="https://www.solidworks.com/" target="_blank">
+                        <img src={Img7} alt="SolidWorks" style={{ width: "300px", height: "auto" }} />
+                    </a>
+                </div>
+                <div className="sp">
+                    <a href="https://www.solidworks.com/" target="_blank">
+                        <img src={Img8} alt="Altium" style={{ width: "300px", height: "auto" }} />
+                    </a>
+                </div>
+            </div> */}
+            {/* <button className="btn">Sign up to be one</button> */}
+        </div>
     </div>
   )
 }
